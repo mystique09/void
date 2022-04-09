191f8c21b189f15adf3c83dc57c3ce7cd6171dbd
mystique09 <knightkrypto7@gmail.com>
2022-04-09 12:52:38 +0800

    add: general commands

---
+++ src/commands/interactions/general.rs
@@ -0,0 +1,47 @@
+use serenity::{
+    client::Context,
+    framework::standard::{
+        macros::{command, group},
+        CommandResult,
+    },
+    model::channel::Message,
+};
+
+#[group]
+#[description = "Group of general commands."]
+#[summary = "General commands."]
+#[commands(ping, avatar, rank, leaderboard)]
+pub struct GeneralCommands;
+
+#[command]
+#[description = "Ping command."]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply_ping(ctx, "Pong!").await?;
+    Ok(())
+}
+
+#[command]
+#[description = "Fetch the user's rak."]
+async fn rank(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply_ping(ctx, "Rank #1").await?;
+    Ok(())
+}
+
+#[command]
+#[description = "Fetch the user's avatar command."]
+async fn avatar(ctx: &Context, msg: &Message) -> CommandResult {
+    let avatar_url = msg
+        .author
+        .avatar_url()
+        .unwrap_or(msg.author.default_avatar_url());
+
+    msg.reply_ping(ctx, avatar_url).await?;
+    Ok(())
+}
+
+#[command]
+#[description = "Get the server's leaderboard."]
+async fn leaderboard(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply_ping(ctx, "Leaderboard.").await?;
+    Ok(())
+}
