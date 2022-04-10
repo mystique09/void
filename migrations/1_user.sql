b0ee0a4caa2393acc862761054c70dd3ecb2fc01
mystique09 <knightkrypto7@gmail.com>
2022-04-10 11:14:11 +0800

    feat: inventory referenfe

--- migrations/1_user.sql
+++ migrations/1_user.sql
@@ -1,7 +1,8 @@
-CREATE TABLE "user" (
+CREATE TABLE IF NOT EXISTS "user" (
   user_id uuid primary key default uuid_generate_v1mc(),
   dc_id bigint not null unique,
   user_rank integer default 0,
   user_balance bigint default 0,
+  inventory_id serial references "inventory"(inventory_id),
   created_at timestamp not null default now()
 );
