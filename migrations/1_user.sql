7f08dd44781cf387c444ba7eed08ab4740f2adbc
mystique09 <knightkrypto7@gmail.com>
2022-04-10 13:35:37 +0800

    add: changed id type to serial, remove: inventoryid

--- migrations/1_user.sql
+++ migrations/1_user.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS "user" (
-  user_id uuid primary key default uuid_generate_v1mc(),
+  user_id serial primary key,
   dc_id bigint not null unique,
   user_rank integer default 0,
   user_balance bigint default 0,
-  inventory_id serial references "inventory"(inventory_id),
+  -- inventory_id serial references "inventory"(inventory_id),
   created_at timestamp not null default now()
 );
