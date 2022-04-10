04830c7527cd70b2eff04db50b95110b46b09e1e
mystique09 <knightkrypto7@gmail.com>
2022-04-10 15:22:45 +0800

    feat: simple query

--- src/db/users.rs
+++ src/db/users.rs
@@ -1,11 +1,11 @@
 use std::sync::Arc;
 
 use serenity::async_trait;
-use sqlx::postgres::PgPool;
+use sqlx::{postgres::PgPool, Executor};
 
 #[derive(Debug)]
 pub struct User {
-    dc_bot: i64,
+    dc_id: i64,
     user_rank: Option<i32>,
     user_balance: Option<u32>,
 }
@@ -13,8 +13,8 @@ pub struct User {
 #[mockall::automock]
 #[async_trait]
 pub trait UserRepo {
-    async fn get_users() -> anyhow::Result<i64>;
-    async fn get_user(id: u64) -> anyhow::Result<i64>;
+    async fn get_users(&self) -> anyhow::Result<i64>;
+    async fn get_user(&self, id: i64) -> anyhow::Result<i64>;
 }
 
 pub struct PgRepo {
@@ -31,10 +31,28 @@ impl PgRepo {
 
 #[async_trait]
 impl UserRepo for PgRepo {
-    async fn get_users() -> anyhow::Result<i64> {
+    async fn get_users(&self) -> anyhow::Result<i64> {
+        let _rec = sqlx::query!(
+            r#"SELECT dc_id, user_rank, user_balance
+        FROM "user" ORDER BY dc_id"#
+        )
+        .fetch_all(&*self.pg_pool)
+        .await?;
+
         Ok(2)
     }
-    async fn get_user(_id: u64) -> anyhow::Result<i64> {
-        Ok(2)
+    async fn get_user(&self, _id: i64) -> anyhow::Result<i64> {
+        let rec = sqlx::query!(
+            r#"
+        SELECT dc_id, user_rank, user_balance
+        FROM "user"
+        WHERE dc_id=$1
+        "#,
+            _id
+        )
+        .fetch_one(&*self.pg_pool)
+        .await?;
+
+        Ok(rec.dc_id)
     }
 }
