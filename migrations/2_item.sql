e470d7e5671981b338b1fa8e4dc16349b1bffd29
mystique09 <knightkrypto7@gmail.com>
2022-04-10 11:14:33 +0800

    add: item schema

---
+++ migrations/2_item.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS "item" (
+  item_id serial primary key not null,
+  item_name VARCHAR(20) unique not null,
+  item_price bigint default 100,
+  item_category VARCHAR(20) not null,
+  inventory_id serial references "inventory"(inventory_id)
+);
