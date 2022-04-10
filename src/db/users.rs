1602e5516a21613b6435806259f4c6f4b3e53535
mystique09 <knightkrypto7@gmail.com>
2022-04-10 15:08:02 +0800

    feat: starter code for db and user

--- src/db/users.rs
+++ src/db/users.rs
@@ -1,54 +1,40 @@
 use std::sync::Arc;
 
 use serenity::async_trait;
-use sqlx::{error::DatabaseError, postgres::PgPool};
+use sqlx::postgres::PgPool;
+
+#[derive(Debug)]
+pub struct User {
+    dc_bot: i64,
+    user_rank: Option<i32>,
+    user_balance: Option<u32>,
+}
 
 #[mockall::automock]
 #[async_trait]
 pub trait UserRepo {
-    async fn add_user(&self, id: u64) -> Result<u64, Box<dyn DatabaseError>>;
-    async fn get_user(&self, id: u64) -> Result<u64, Box<dyn DatabaseError>>;
+    async fn get_users() -> anyhow::Result<i64>;
+    async fn get_user(id: u64) -> anyhow::Result<i64>;
 }
 
-pub struct PostgresUserRepo {
-    pg_pool: Arc<PgPool>,
+pub struct PgRepo {
+    pub pg_pool: Arc<PgPool>,
 }
 
-impl PostgresUserRepo {
-    pub fn new(pg_pool: PgPool) -> Self {
+impl PgRepo {
+    pub fn new(pool: PgPool) -> Self {
         Self {
-            pg_pool: Arc::new(pg_pool),
+            pg_pool: Arc::new(pool),
         }
     }
 }
 
 #[async_trait]
-impl UserRepo for PostgresUserRepo {
-    async fn add_user(&self, id: u64) -> Result<u64, Box<dyn DatabaseError>> {
-        let query = sqlx::query!(
-            r#"
-       INSERT INTO "user" (dc_id)
-       VALUES ($1)
-       RETURNING user_id
-       "#,
-            id
-        )
-        .fetch_one(&*self.pg_pool)
-        .await;
-
-        query
+impl UserRepo for PgRepo {
+    async fn get_users() -> anyhow::Result<i64> {
+        Ok(2)
     }
-    async fn get_user(&self, id: u64) -> Result<u64, Box<dyn DatabaseError>> {
-        let query = sqlx::query!(
-            r#"
-        SELECT * FROM "user"
-        WHERE id=$1
-        "#,
-            id
-        )
-        .fetch_one(&*self.pg_pool)
-        .await;
-
-        query
+    async fn get_user(_id: u64) -> anyhow::Result<i64> {
+        Ok(2)
     }
 }
