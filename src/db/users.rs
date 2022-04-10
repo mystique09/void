f90045037565f763acea53e1c04cab82f9daa346
mystique09 <knightkrypto7@gmail.com>
2022-04-10 12:29:41 +0800

    feat: remove query, useless(for now)

--- src/db/users.rs
+++ src/db/users.rs
@@ -1,12 +1,5 @@
 use sqlx::postgres::PgPool;
 
-pub async fn create_user(db: &PgPool, user_id: i64) {
-    let new_user = sqlx::query!(
-        r#"insert into "user" (dc_id) values($1) returning user_id"#,
-        user_id,
-    )
-    .fetch_one(db)
-    .await;
-}
+pub async fn create_user(db: &PgPool, user_id: i64) {}
 
 pub async fn get_user(db: &PgPool) {}
