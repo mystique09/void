8e9bbdde1541054489a55567a626529820f02703
mystique09 <knightkrypto7@gmail.com>
2022-04-10 13:36:03 +0800

    add: basic user query

--- src/db/users.rs
+++ src/db/users.rs
@@ -1,5 +1,54 @@
-use sqlx::postgres::PgPool;
+use std::sync::Arc;
 
-pub async fn create_user(db: &PgPool, user_id: i64) {}
+use serenity::async_trait;
+use sqlx::{error::DatabaseError, postgres::PgPool};
 
-pub async fn get_user(db: &PgPool) {}
+#[mockall::automock]
+#[async_trait]
+pub trait UserRepo {
+    async fn add_user(&self, id: u64) -> Result<u64, Box<dyn DatabaseError>>;
+    async fn get_user(&self, id: u64) -> Result<u64, Box<dyn DatabaseError>>;
+}
+
+pub struct PostgresUserRepo {
+    pg_pool: Arc<PgPool>,
+}
+
+impl PostgresUserRepo {
+    pub fn new(pg_pool: PgPool) -> Self {
+        Self {
+            pg_pool: Arc::new(pg_pool),
+        }
+    }
+}
+
+#[async_trait]
+impl UserRepo for PostgresUserRepo {
+    async fn add_user(&self, id: u64) -> Result<u64, Box<dyn DatabaseError>> {
+        let query = sqlx::query!(
+            r#"
+       INSERT INTO "user" (dc_id)
+       VALUES ($1)
+       RETURNING user_id
+       "#,
+            id
+        )
+        .fetch_one(&*self.pg_pool)
+        .await;
+
+        query
+    }
+    async fn get_user(&self, id: u64) -> Result<u64, Box<dyn DatabaseError>> {
+        let query = sqlx::query!(
+            r#"
+        SELECT * FROM "user"
+        WHERE id=$1
+        "#,
+            id
+        )
+        .fetch_one(&*self.pg_pool)
+        .await;
+
+        query
+    }
+}
