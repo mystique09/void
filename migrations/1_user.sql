60e8709adc7e503b327704624b93c4c29198dd14
mystique09 <knightkrypto7@gmail.com>
2022-04-11 19:28:53 +0800

    add: user_name in sql field

--- migrations/1_user.sql
+++ migrations/1_user.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS "user" (
   user_id bigserial primary key,
+  user_name varchar(30) not null unique,
   dc_id bigint not null unique,
   user_rank bigint not null default 0,
   user_exp bigint not null default 0,
