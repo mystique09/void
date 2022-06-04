ace1f52613beda8e38902bd2126caeb1c025e105
mystique09 <knightkrypto7@gmail.com>
2022-06-04 15:26:00 +0800

    add: kick, clean, ban command

--- src/commands/interactions/admin.rs
+++ src/commands/interactions/admin.rs
@@ -1,4 +1,5 @@
 use serenity::framework::standard::macros::group;
+use serenity::framework::standard::Args;
 use serenity::{
     client::Context,
     framework::standard::{macros::command, CommandResult},
@@ -15,21 +16,63 @@ pub struct AdminCommands;
 
 #[command]
 #[description = "Use to clean bulk of messages."]
-pub async fn clean(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "[todo!] cleaned 100 messagges.").await?;
+pub async fn clean(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    // get the amount of messages to delete ffrom argument , default is 11
+    let amount = args.single::<u64>().unwrap_or(11);
+
+    // prune the messages  based on the amount
+    let _ = msg
+        .channel_id
+        .messages(&ctx.http, |m| m.limit(amount))
+        .await;
+
     Ok(())
 }
 
 #[command]
 #[description = "Ban a user."]
-pub async fn ban(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "[todo!] Ban user command.").await?;
+pub async fn ban(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    // get the mentioned user in the argument
+    let user = match args.single::<u64>() {
+        Ok(user) => user,
+        Err(_) => {
+            msg.reply(ctx, "Please mention a user to ban.").await?;
+            return Ok(());
+        }
+    };
+
+    // ban the user
+    let _ = msg.guild_id.unwrap().ban(&ctx.http, user, 4).await;
+    // Notify the server owner that a user has been banned
+    // mention the user that has been banned
+    let _ = msg
+        .channel_id
+        .say(&ctx.http, format!("{} has been banned.", user))
+        .await;
+
     Ok(())
 }
 
 #[command]
 #[description = "Kick a user"]
-pub async fn kick(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "[todo!] Kick a user command.").await?;
+pub async fn kick(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    // get the mentioned user in the argument
+    let user = match args.single::<u64>() {
+        Ok(user) => user,
+        Err(_) => {
+            msg.reply(ctx, "Please mention a user to kick.").await?;
+            return Ok(());
+        }
+    };
+
+    // kick the user
+    let _ = msg.guild_id.unwrap().kick(&ctx.http, user).await;
+    // Notify the server owner that a user has been kicked
+    // mention the user that has been kicked
+    let _ = msg
+        .channel_id
+        .say(&ctx.http, format!("{} has been kicked.", user))
+        .await;
+
     Ok(())
 }
