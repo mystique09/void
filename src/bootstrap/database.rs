98448dd95e6a59745f81572f4e65c0920b44ba28
benjie <50514928+mystique09@users.noreply.github.com>
2023-02-14 07:49:09 +0000

    add: database bootstrap module

---
+++ src/bootstrap/database.rs
@@ -0,0 +1,21 @@
+use sqlx::{PgPool, Postgres, Pool};
+
+#[derive(Clone)]
+pub struct Database {
+    pub pool: Pool<Postgres>,
+}
+
+impl Database {
+    pub async fn new(env: &super::env::Env) -> Self {
+        let db_url = env.get_db_url();
+        let pool = PgPool::connect(&db_url).await.unwrap();
+
+        if let Err(value) = sqlx::migrate!().run(&pool).await {
+            println!("Version mismatch {:?}", value);
+        }
+
+        Self {
+            pool,
+        }
+    }
+}
