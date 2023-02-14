036b8207ce043745b423393e174acd74f3adaf19
benjie <50514928+mystique09@users.noreply.github.com>
2023-02-14 07:49:33 +0000

    add: application

---
+++ src/bootstrap/application.rs
@@ -0,0 +1,24 @@
+use super::database::Database;
+use super::env::Env;
+use crate::bot::bot::Bot;
+
+pub struct Application {
+    pub env: Env,
+    pub db: Database,
+    pub bot: Bot,
+}
+
+impl Application {
+    pub async fn new() -> Self {
+        let env = Env::new().unwrap();
+        let db = Database::new(&env).await;
+        let bot = Bot::new(&env).await;
+
+        Self { env, db, bot }
+    }
+
+    pub async fn start(&mut self) {
+        self.bot.write_data(&self.db).await;
+        self.bot.start().await;
+    }
+}
