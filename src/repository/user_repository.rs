38dbde12a83ad96a4a6751a5cb83dfd43064328b
benjie <50514928+mystique09@users.noreply.github.com>
2023-02-14 07:51:32 +0000

    add: repository

---
+++ src/repository/user_repository.rs
@@ -0,0 +1,25 @@
+use crate::domain;
+
+pub struct UserRepository {}
+
+impl domain::user::UserRepository for UserRepository {
+    fn get_users() -> Result<Vec<domain::user::User>, String> {
+        todo!()
+    }
+
+    fn get_user_by_id(_id: i32) -> Option<domain::user::User> {
+        todo!()
+    }
+
+    fn get_user_by_username(_username: &str) -> Option<domain::user::User> {
+        todo!()
+    }
+
+    fn set_user_exp(_new_exp: i32) -> Result<i32, String> {
+        todo!()
+    }
+
+    fn delete_user(_id: i32) -> Result<domain::user::User, String> {
+        todo!()
+    }
+}
