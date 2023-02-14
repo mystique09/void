0f42161819aeb852c7379f64ea81c261cc0dcc54
benjie <50514928+mystique09@users.noreply.github.com>
2023-02-14 08:04:19 +0000

    add: ready event

--- src/bot/handler.rs
+++ src/bot/handler.rs
@@ -1,5 +1,13 @@
-use serenity::{async_trait, prelude::EventHandler};
+use serenity::{async_trait, prelude::{EventHandler, Context}, model::{prelude::{Ready, Activity}, user::OnlineStatus}};
 
 pub struct Handler;
+
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        let activity: Option<Activity> = Some(Activity::playing("NeoVim"));
+
+        ctx.set_presence(activity, OnlineStatus::Online).await;
+        println!("{} is now open.", &ready.user.name);
+    }
+}
