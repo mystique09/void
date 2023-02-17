2a42c80079b94be0df7bdd942728cb6ae5881ec9
Benjie Ben Garcia <knightkrypto7@gmail.com>
2023-02-17 05:51:49 +0000

    add: user usecase module

--- src/usecase/user_usecase.rs
+++ src/usecase/user_usecase.rs
@@ -1,25 +1,45 @@
+use anyhow::Result;
+use serenity::async_trait;
+use sqlx::Error as SqlxError;
+
 use crate::domain;
 
-pub struct UserUsecase {}
+pub struct UserUsecase<'a> {
+    pub repository: &'a dyn domain::user::UserRepository,
+}
 
-impl domain::user::UserUsecase for UserUsecase {
-    fn get_users() -> Result<Vec<domain::user::User>, String> {
-        todo!()
+impl<'a> UserUsecase<'a> {
+    pub fn new(repository: &'a dyn domain::user::UserRepository) -> Self {
+        Self { repository }
+    }
+}
+
+#[async_trait(?Send)]
+impl<'a> domain::user::UserUsecase for UserUsecase<'a> {
+    async fn create_user(
+        &self,
+        data: domain::user::CreateUserDTO,
+    ) -> Result<domain::user::User, SqlxError> {
+        self.repository.create_user(data).await
     }
 
-    fn get_user_by_id(_id: i32) -> Option<domain::user::User> {
-        todo!()
+    async fn get_users(&self) -> Result<Vec<domain::user::User>, SqlxError> {
+        self.repository.get_users().await
     }
 
-    fn get_user_by_username(_username: &str) -> Option<domain::user::User> {
-        todo!()
+    async fn get_user_by_id(&self, id: i64) -> Result<domain::user::User, SqlxError> {
+        self.repository.get_user_by_id(id).await
     }
 
-    fn set_user_exp(_new_exp: i32) -> Result<i32, String> {
-        todo!()
+    async fn get_user_by_username(&self, username: &str) -> Result<domain::user::User, SqlxError> {
+        self.repository.get_user_by_username(username).await
     }
 
-    fn delete_user(_id: i32) -> Result<domain::user::User, String> {
-        todo!()
+    async fn set_user_exp(&self, new_exp: i32) -> Result<bool> {
+        self.repository.set_user_exp(new_exp).await
+    }
+
+    async fn delete_user(&self, id: i64) -> Result<bool> {
+        self.repository.delete_user(id).await
     }
 }
