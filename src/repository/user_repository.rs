b689b0c2df21e9b3775426e6618a2a71f60f744b
Benjie Ben Garcia <knightkrypto7@gmail.com>
2023-02-17 05:51:43 +0000

    add: user repository module

--- src/repository/user_repository.rs
+++ src/repository/user_repository.rs
@@ -1,25 +1,100 @@
-use crate::domain;
+use crate::{bootstrap::database::Database, domain};
+use anyhow::Result;
+use serenity::async_trait;
+use sqlx::Error as SqlxError;
 
-pub struct UserRepository {}
+pub struct UserRepository<'a> {
+    pub conn: &'a Database,
+}
 
-impl domain::user::UserRepository for UserRepository {
-    fn get_users() -> Result<Vec<domain::user::User>, String> {
+impl<'a> UserRepository<'a> {
+    pub fn new(conn: &'a Database) -> Self {
+        Self { conn }
+    }
+}
+
+#[async_trait]
+impl<'a> domain::user::UserRepository for UserRepository<'a> {
+    async fn create_user(
+        &self,
+        data: domain::user::CreateUserDTO,
+    ) -> Result<domain::user::User, SqlxError> {
+        let query = sqlx::query_as!(
+            domain::user::User,
+            r#"
+        INSERT INTO "users" (
+            id, nickname
+        ) VALUES (
+            $1, $2
+        )
+        RETURNING *
+        "#,
+            data.id,
+            data.nickname
+        )
+        .fetch_one(&self.conn.pool)
+        .await;
+
+        query
+    }
+
+    async fn get_users(&self) -> Result<Vec<domain::user::User>, SqlxError> {
+        let query = sqlx::query_as!(
+            domain::user::User,
+            r#"
+        SELECT * FROM "users"
+        "#
+        )
+        .fetch_all(&self.conn.pool)
+        .await;
+
+        query
+    }
+
+    async fn get_user_by_id(&self, id: i64) -> Result<domain::user::User, SqlxError> {
+        let query = sqlx::query_as!(
+            domain::user::User,
+            r#"
+            SELECT * FROM "users"
+            WHERE id = $1
+            "#,
+            id
+        )
+        .fetch_one(&self.conn.pool)
+        .await;
+        query
+    }
+
+    async fn get_user_by_username(&self, username: &str) -> Result<domain::user::User, SqlxError> {
+        let query = sqlx::query_as!(
+            domain::user::User,
+            r#"
+            SELECT * FROM "users"
+            WHERE nickname = $1
+            "#,
+            username
+        )
+        .fetch_one(&self.conn.pool)
+        .await;
+        query
+    }
+
+    async fn set_user_exp(&self, _new_exp: i32) -> Result<bool> {
         todo!()
     }
 
-    fn get_user_by_id(_id: i32) -> Option<domain::user::User> {
-        todo!()
-    }
+    async fn delete_user(&self, id: i64) -> Result<bool> {
+        let query = sqlx::query!(
+            r#"
+                DELETE FROM "users"
+                WHERE id = $1
+                "#,
+            id
+        )
+        .execute(&__self.conn.pool)
+        .await?
+        .rows_affected();
 
-    fn get_user_by_username(_username: &str) -> Option<domain::user::User> {
-        todo!()
-    }
-
-    fn set_user_exp(_new_exp: i32) -> Result<i32, String> {
-        todo!()
-    }
-
-    fn delete_user(_id: i32) -> Result<domain::user::User, String> {
-        todo!()
+        Ok(query > 0)
     }
 }
