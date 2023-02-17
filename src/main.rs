55ed40917742a62711b1d6cbb460be5bf7a8d4df
Benjie Ben Garcia <knightkrypto7@gmail.com>
2023-02-17 07:17:04 +0000

    add: tracing env logger

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use void::bootstrap::application::Application;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     let mut app = Application::new().await;
     app.start().await;
 }
