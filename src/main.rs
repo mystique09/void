9f31694cb038e63f4f0d8ec6704c079b2965e685
mystique09 <knightkrypto7@gmail.com>
2023-02-18 10:15:48 +0800

    fix: load the .sample.env

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use void::bootstrap::application::Application;
 
 #[tokio::main]
 async fn main() {
+    dotenv::from_filename(".sample.env").ok();
     tracing_subscriber::fmt::init();
 
     let mut app = Application::new().await;
