2b573b3e3e74d37a9afe459f4d598b31aac98d55
mystique09 <knightkrypto7@gmail.com>
2023-02-18 21:40:13 +0800

    add: match_app_command fn

--- src/bot/commands/app_commands/mod.rs
+++ src/bot/commands/app_commands/mod.rs
@@ -1,4 +1,24 @@
+use std::sync::Arc;
+
+use serenity::{
+    model::prelude::interaction::application_command::{
+        ApplicationCommandInteraction, CommandDataOption,
+    },
+    prelude::Context,
+};
+
 pub mod admin;
 pub mod auto_respond;
 pub mod bump;
 pub mod rss;
+
+pub async fn match_app_command(
+    ctx: &Arc<Context>,
+    cmd: &ApplicationCommandInteraction,
+    options: &[CommandDataOption],
+) -> String {
+    match cmd.data.name.as_str() {
+        "bump" => bump::create_bump::run(Arc::clone(ctx), cmd, options).await,
+        _ => "not implemented".to_string(),
+    }
+}
