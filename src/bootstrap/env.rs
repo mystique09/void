e540d2baa1d0ca5f873b23880a47c53bd810c234
benjie <50514928+mystique09@users.noreply.github.com>
2023-02-18 08:32:07 +0000

    fix: loading env

--- src/bootstrap/env.rs
+++ src/bootstrap/env.rs
@@ -1,35 +1,46 @@
 use std::env;
 use tracing::{info, error};
 
+#[derive(Clone)]
 pub struct Env {
-    pub db_url: Option<String>,
-    pub token: Option<String>,
-    pub guild_id: Option<String>,
+    db_url: Option<String>,
+    token: Option<String>,
+    guild_id: Option<String>,
+    channel_id: Option<u64>,
+    app_id: Option<String>,
+    mode: Option<String>
 }
 
 impl Env {
     pub async fn new() -> Self {
-        match dotenv::from_filename(".sample.env").ok() {
-            Some(prod_env) => info!("production environment variable loaded: {:?}", prod_env),
+        match dotenv::dotenv().ok() {
+            Some(dev_env) => info!("development environment variable loaded: {:?}", dev_env),
             None => {
-                info!("no .sample.env file detected, loading .env file");
+                info!("no .env file detected, loading .env file");
                 
-                match dotenv::dotenv().ok() {
-                    Some(dev_env) => {
-                        info!("development environment variable loaded: {:?}", dev_env);
+                match dotenv::from_filename(".sample.env").ok() {
+                    Some(prod_env) => {
+                        info!("production environment variable loaded: {:?}", prod_env);
                     },
-                    None => error!("no .env file detected"),
+                    None => error!("no .sample.env file detected"),
                 }
             }
         }
 
         let database_url = env::var("DATABASE_URL").unwrap();
         let token = env::var("TOKEN").unwrap();
+        let channel_id = env::var("CHANNEL_ID").unwrap();
+        let app_id = env::var("APP_ID").unwrap();
+        let mode = env::var("MODE").unwrap();
+        let parse_channel_id = u64::from_str_radix(&channel_id, 10).unwrap();
 
         Self {
             db_url: Some(database_url),
             token: Some(token),
             guild_id: None,
+            channel_id: Some(parse_channel_id),
+            app_id: Some(app_id),
+            mode: Some(mode)
         }
     }
 
@@ -43,5 +54,17 @@ impl Env {
 
     pub fn get_guild_id(&self) -> &String {
         self.guild_id.as_ref().expect("no guild id")
+    }
+
+    pub fn get_channel_id(&self) -> &u64 {
+        self.channel_id.as_ref().expect("no channel id")
+    }
+
+    pub fn get_app_id(&self) -> &String {
+        self.app_id.as_ref().expect("no app id")
+    }
+
+    pub fn get_mode(&self) -> &String {
+        self.mode.as_ref().expect("no mode")
     }
 }
