5d27a9582eb43addd068a6e9013a686cdb5de085
mystique09 <knightkrypto7@gmail.com>
2023-02-27 03:29:16 +0800

    add: methods

--- src/repository/keyword_repository.rs
+++ src/repository/keyword_repository.rs
@@ -1,1 +1,56 @@
+use serenity::async_trait;
 
+use crate::bootstrap::database::Database;
+use crate::domain::auto_respond;
+use anyhow::Result;
+use sqlx::Error as SqlxError;
+
+pub struct KeywordRepository {
+    pub conn: Database,
+}
+
+impl KeywordRepository {
+    pub fn new(conn: Database) -> Self {
+        Self { conn }
+    }
+}
+
+#[async_trait]
+impl auto_respond::KeywordRepository for KeywordRepository {
+    async fn create_keyword(
+        &self,
+        data: auto_respond::CreateKeywordDTO,
+    ) -> Result<auto_respond::Keyword, SqlxError> {
+        let query = sqlx::query_as!(
+            auto_respond::Keyword,
+            r#"
+        INSERT INTO "keywords" (
+            id, word, response, response_type, response_mode   
+        ) VALUES (
+            $1, $2, $3, $4, $5
+        ) RETURNING *;
+        "#,
+            data.id,
+            data.word,
+            data.response,
+            data.response_type,
+            data.response_mode
+        )
+        .fetch_one(&self.conn.pool)
+        .await;
+
+        query
+    }
+    async fn get_keyword(&self, id: i64) -> Result<auto_respond::Keyword, SqlxError> {
+        todo!()
+    }
+    async fn get_keywords(&self) -> Result<Vec<auto_respond::Keyword>, SqlxError> {
+        todo!()
+    }
+    async fn update_keyword(&self, id: i64, data: auto_respond::UpdateKeywordDTO) -> Result<bool> {
+        todo!()
+    }
+    async fn delete_keyword(&self, id: i64) -> Result<bool> {
+        todo!()
+    }
+}
--- src/usecase/keyword_usecase.rs
+++ src/usecase/keyword_usecase.rs
@@ -1,1 +1,38 @@
+use serenity::async_trait;
 
+use crate::domain::auto_respond;
+use crate::repository::keyword_repository;
+use anyhow::Result;
+use sqlx::Error as SqlxError;
+
+pub struct KeywordUsecase {
+    pub repository: keyword_repository::KeywordRepository,
+}
+
+impl KeywordUsecase {
+    pub fn new(repository: keyword_repository::KeywordRepository) -> Self {
+        Self { repository }
+    }
+}
+
+#[async_trait(?Send)]
+impl auto_respond::KeywordUsecase for KeywordUsecase {
+    async fn create_keyword(
+        &self,
+        data: auto_respond::CreateKeywordDTO,
+    ) -> Result<auto_respond::Keyword, SqlxError> {
+        todo!()
+    }
+    async fn get_keyword(&self, id: i64) -> Result<auto_respond::Keyword, SqlxError> {
+        todo!()
+    }
+    async fn get_keywords(&self) -> Result<Vec<auto_respond::Keyword>, SqlxError> {
+        todo!()
+    }
+    async fn update_keyword(&self, id: i64, data: auto_respond::UpdateKeywordDTO) -> Result<bool> {
+        todo!()
+    }
+    async fn delete_keyword(&self, id: i64) -> Result<bool> {
+        todo!()
+    }
+}
