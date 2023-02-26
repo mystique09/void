b83ded8a095971c3126c074b7de6537a3a6d97aa
mystique09 <knightkrypto7@gmail.com>
2023-02-26 20:56:10 +0800

    feature: register bum_cancel global command

--- src/bot/commands/app_commands/mod.rs
+++ src/bot/commands/app_commands/mod.rs
@@ -21,17 +21,25 @@ pub async fn match_app_command(
 ) -> String {
     match cmd.data.name.as_str() {
         "bump" => bump::create_bump::run(Arc::clone(ctx), cmd, options).await,
+        "bump_cancel" => bump::cancel_bump::run(Arc::clone(ctx), cmd, options).await,
         _ => "not implemented".to_string(),
     }
 }
 
 pub async fn register_commands(ctx: &Context) {
-    match Command::create_global_application_command(&ctx.http, |command| {
+    Command::create_global_application_command(&ctx.http, |command| {
         bump::create_bump::register(command)
     })
     .await
-    {
-        Ok(command) => info!("Created global app command: {}", command.name),
-        Err(why) => error!("Error creating global command: {}", why),
-    };
+    .map(|command| info!("created global command: {}", command.name))
+    .map_err(|why| error!("error creating global command: {}", why))
+    .unwrap();
+
+    Command::create_global_application_command(&ctx.http, |command| {
+        bump::cancel_bump::register(command)
+    })
+    .await
+    .map(|command| info!("created global command: {}", command.name))
+    .map_err(|why| error!("error creating global command: {}", why))
+    .unwrap();
 }
