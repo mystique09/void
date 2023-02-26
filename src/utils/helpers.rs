eef97fbff170e992ddc84567d7c9b5256d43b5ce
mystique09 <knightkrypto7@gmail.com>
2023-02-26 18:14:19 +0800

    fix: use char

--- src/utils/helpers.rs
+++ src/utils/helpers.rs
@@ -5,14 +5,14 @@ pub const BUMPS: &str = include_str!("bumps.txt");
 pub const RESPONSES: &str = include_str!("response.txt");
 
 pub async fn generate_random_bump() -> &'static str {
-    let bumps = BUMPS.split("\n").collect::<Vec<&str>>();
+    let bumps = BUMPS.split('\n').collect::<Vec<&str>>();
     let i = randn(0..(bumps.len() as u32)).await;
 
     bumps[i as usize]
 }
 
 pub async fn generate_random_response() -> &'static str {
-    let responses = RESPONSES.split("\n").collect::<Vec<&str>>();
+    let responses = RESPONSES.split('\n').collect::<Vec<&str>>();
     let i = randn(0..(responses.len() as u32)).await;
 
     responses[i as usize]
