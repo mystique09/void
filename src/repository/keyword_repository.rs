6f32e70183a956e4ddaae642f3e1f19bd3fa41d7
mystique09 <knightkrypto7@gmail.com>
2023-02-27 11:00:39 +0800

    add: impl KeywordRepository

--- src/repository/keyword_repository.rs
+++ src/repository/keyword_repository.rs
@@ -21,36 +21,127 @@ impl auto_respond::KeywordRepository for KeywordRepository {
         &self,
         data: auto_respond::CreateKeywordDTO,
     ) -> Result<auto_respond::Keyword, SqlxError> {
-        let query = sqlx::query_as!(
-            auto_respond::Keyword,
+        let query = sqlx::query_as!(auto_respond::Keyword,
             r#"
         INSERT INTO "keywords" (
             id, word, response, response_type, response_mode   
         ) VALUES (
             $1, $2, $3, $4, $5
-        ) RETURNING *;
+        ) RETURNING id, word, response, response_type as "response_type: _", response_mode as "response_mode: _", created_at, updated_at;
         "#,
             data.id,
             data.word,
             data.response,
-            data.response_type,
-            data.response_mode
+            data.response_type as _,
+            data.response_mode as _
         )
         .fetch_one(&self.conn.pool)
         .await;
 
         query
     }
+
     async fn get_keyword(&self, id: i64) -> Result<auto_respond::Keyword, SqlxError> {
-        todo!()
+        let query = sqlx::query_as!(
+            auto_respond::Keyword,
+            r#"
+        SELECT id, word, response, response_type as "response_type: _", response_mode as "response_mode: _", created_at, updated_at
+        FROM "keywords"
+        WHERE id = $1;
+            "#,
+            id
+        )
+        .fetch_one(&self.conn.pool)
+        .await;
+
+        query
     }
+
     async fn get_keywords(&self) -> Result<Vec<auto_respond::Keyword>, SqlxError> {
-        todo!()
+        let query = sqlx::query_as!(
+            auto_respond::Keyword,
+            r#"
+        SELECT id, word, response, response_type as "response_type: _", response_mode as "response_mode: _", created_at, updated_at
+        FROM "keywords";
+            "#
+        )
+        .fetch_all(&self.conn.pool)
+        .await;
+
+        query
     }
-    async fn update_keyword(&self, id: i64, data: auto_respond::UpdateKeywordDTO) -> Result<bool> {
-        todo!()
+
+    async fn update_response(&self, id: i64, new_response: &str) -> Result<bool> {
+        let query = sqlx::query!(
+            r#"
+            UPDATE "keywords"
+            SET response = $2
+            WHERE id = $1;
+            "#,
+            id,
+            new_response
+        )
+        .execute(&self.conn.pool)
+        .await?
+        .rows_affected();
+
+        Ok(query > 0)
     }
+
+    async fn update_response_type(
+        &self,
+        id: i64,
+        new_response_type: auto_respond::ResponseType,
+    ) -> Result<bool> {
+        let query = sqlx::query!(
+            r#"
+            UPDATE "keywords"
+            SET response_type = $2
+            WHERE id = $1;
+            "#,
+            id,
+            new_response_type as _
+        )
+        .execute(&self.conn.pool)
+        .await?
+        .rows_affected();
+
+        Ok(query > 0)
+    }
+
+    async fn update_response_mode(
+        &self,
+        id: i64,
+        new_response_mode: auto_respond::ResponseMode,
+    ) -> Result<bool> {
+        let query = sqlx::query!(
+            r#"
+            UPDATE "keywords"
+            SET response_mode = $2
+            WHERE id = $1;
+            "#,
+            id,
+            new_response_mode as _
+        )
+        .execute(&self.conn.pool)
+        .await?
+        .rows_affected();
+
+        Ok(query > 0)
+    }
+
     async fn delete_keyword(&self, id: i64) -> Result<bool> {
-        todo!()
+        let query = sqlx::query!(
+            r#"
+            DELETE FROM "keywords"
+            WHERE id = $1;
+            "#,
+            id
+        )
+        .execute(&self.conn.pool)
+        .await?
+        .rows_affected();
+
+        Ok(query > 0)
     }
 }
