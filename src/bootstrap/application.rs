e9391d90c9671d994d89399a40665f2be3a68944
mystique09 <knightkrypto7@gmail.com>
2023-02-27 11:02:12 +0800

    feat: add usecase for keywords, and format

--- src/bootstrap/application.rs
+++ src/bootstrap/application.rs
@@ -8,7 +8,9 @@ use serenity::{
 
 use super::database::Database;
 use super::env::Env;
-use crate::bot::shared::{Guild, SharedBumpState, SharedEnvState, SharedGuildState};
+use crate::bot::shared::{
+    Guild, SharedBumpState, SharedEnvState, SharedGuildState, SharedKeywordUsecase,
+};
 use crate::{
     bot::{init::Bot, shared::SharedUserUsecase},
     repository, usecase,
@@ -26,21 +28,32 @@ impl Application {
         let db = Database::new(&env).await;
         let bot = Bot::new(&env).await;
 
-        let guild_cache: HashMap<GuildId, Guild> = HashMap::new();
-        let bump_cache: Vec<(UserId, Duration)> = vec![];
+        let shared_env = Arc::new(RwLock::new(env.clone()));
+        let guild_cache = {
+            let guilds: HashMap<GuildId, Guild> = HashMap::new();
+            Arc::new(RwLock::new(guilds))
+        };
+        let bump_cache = {
+            let bumps: Vec<(UserId, Duration)> = vec![];
+            Arc::new(RwLock::new(bumps))
+        };
         let shared_user_usecase = {
             let user_repo = repository::user_repository::UserRepository::new(db.clone());
             let user_case = usecase::user_usecase::UserUsecase::new(user_repo);
             Arc::new(RwLock::new(user_case))
         };
-        let shared_env = Arc::new(RwLock::new(env.clone()));
+        let shared_keyword_usecase = {
+            let keyword_repo = repository::keyword_repository::KeywordRepository::new(db.clone());
+            let keyword_usecase = usecase::keyword_usecase::KeywordUsecase::new(keyword_repo);
+            Arc::new(RwLock::new(keyword_usecase))
+        };
 
         bot.write_data::<SharedEnvState>(shared_env).await;
+        bot.write_data::<SharedGuildState>(guild_cache).await;
+        bot.write_data::<SharedBumpState>(bump_cache).await;
         bot.write_data::<SharedUserUsecase>(shared_user_usecase)
             .await;
-        bot.write_data::<SharedGuildState>(Arc::new(RwLock::new(guild_cache)))
-            .await;
-        bot.write_data::<SharedBumpState>(Arc::new(RwLock::new(bump_cache)))
+        bot.write_data::<SharedKeywordUsecase>(shared_keyword_usecase)
             .await;
 
         Self { env, db, bot }
