a6dbe51aba963d11642eaa16fb9271ac0455e80b
mystique09 <knightkrypto7@gmail.com>
2023-02-28 11:56:35 +0800

    add: auto respond

--- src/bot/commands/app_commands/mod.rs
+++ src/bot/commands/app_commands/mod.rs
@@ -23,6 +23,7 @@ pub async fn match_app_command(
     match cmd.data.name.as_str() {
         "bump" => bump::create_bump::run(Arc::clone(ctx), cmd, options).await,
         "bump_cancel" => bump::cancel_bump::run(Arc::clone(ctx), cmd, options).await,
+        "auto_respond" => auto_respond::set::run(Arc::clone(ctx), cmd, options).await,
         _ => "not implemented".to_string(),
     }
 }
@@ -60,6 +61,14 @@ pub async fn register_global_commands(ctx: &Context) {
 
     Command::create_global_application_command(&ctx.http, |command| {
         bump::cancel_bump::register(command)
+    })
+    .await
+    .map(|command| info!("created global command: {}", command.name))
+    .map_err(|why| error!("error creating global command: {}", why))
+    .unwrap();
+
+    Command::create_global_application_command(&ctx.http, |command| {
+        auto_respond::set::register(command)
     })
     .await
     .map(|command| info!("created global command: {}", command.name))
