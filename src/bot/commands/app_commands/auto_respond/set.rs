e79f5661340e7017f985e3dc7bbc1936079125b2
mystique09 <knightkrypto7@gmail.com>
2023-02-28 16:30:03 +0800

    add: set slash command

--- src/bot/commands/app_commands/auto_respond/set.rs
+++ src/bot/commands/app_commands/auto_respond/set.rs
@@ -1,22 +1,121 @@
 use std::sync::Arc;
 
+use crate::domain::auto_respond::KeywordUsecase;
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::prelude::command::CommandOptionType;
+use serenity::model::prelude::interaction::application_command::CommandDataOptionValue;
 use serenity::{
     model::prelude::interaction::application_command::{
         ApplicationCommandInteraction, CommandDataOption,
     },
     prelude::Context,
 };
-//use tracing::{error, info};
+use tracing::{error, info};
+
+use crate::bot::shared::{SharedKeywordUsecase, SharedKeywordsState};
 
 pub async fn run(
-    _ctx: Arc<Context>,
-    _command: &ApplicationCommandInteraction,
-    _options: &[CommandDataOption],
+    ctx: Arc<Context>,
+    command: &ApplicationCommandInteraction,
+    options: &[CommandDataOption],
 ) -> String {
     //todo!("implement set method");
-    "new keyword added".to_string()
+    let new_keyword = {
+        let keyword = {
+            let kw = options
+                .get(0)
+                .expect("keyword")
+                .resolved
+                .as_ref()
+                .expect("keyword");
+
+            if let CommandDataOptionValue::String(v) = kw {
+                v
+            } else {
+                return "missing keyword".to_string();
+            }
+        };
+
+        let response = {
+            let res = options
+                .get(1)
+                .expect("response")
+                .resolved
+                .as_ref()
+                .expect("response");
+
+            if let CommandDataOptionValue::String(v) = res {
+                v
+            } else {
+                return "missing response".to_string();
+            }
+        };
+
+        let response_type = {
+            let res_type = options
+                .get(2)
+                .expect("response type")
+                .resolved
+                .as_ref()
+                .expect("response type");
+
+            if let CommandDataOptionValue::String(v) = res_type {
+                v
+            } else {
+                return "missing response type".to_string();
+            }
+        };
+
+        let response_mode = {
+            let res_mode = options
+                .get(3)
+                .expect("response mode")
+                .resolved
+                .as_ref()
+                .expect("response mode");
+
+            if let CommandDataOptionValue::String(v) = res_mode {
+                v
+            } else {
+                return "missing response type".to_string();
+            }
+        };
+
+        crate::domain::auto_respond::CreateKeywordDTO {
+            id: 1,
+            word: keyword.into(),
+            guild_id: command.guild_id.unwrap().0 as i64,
+            response: response.into(),
+            response_type: crate::domain::auto_respond::ResponseType::from(response_type.as_str()),
+            response_mode: crate::domain::auto_respond::ResponseMode::from(response_mode.as_str()),
+        }
+    };
+
+    let data = ctx.data.read().await;
+    let keyword_usecase_lock = {
+        let kw_lock = data.get::<SharedKeywordUsecase>().unwrap().clone();
+        kw_lock
+    };
+    let keyword_usecase = keyword_usecase_lock.read().await;
+    let keyword_state_lock = {
+        let kw_state_lock = data.get::<SharedKeywordsState>().unwrap().clone();
+        kw_state_lock
+    };
+    let mut keyword_state = keyword_state_lock.write().await;
+
+    match keyword_usecase.create_keyword(new_keyword).await {
+        Ok(v) => {
+            info!("new keyword created {}", &v.word);
+            let guild = keyword_state.get_mut(&command.guild_id.unwrap()).unwrap();
+            guild.push(v);
+
+            "new keyword added".to_string()
+        }
+        Err(why) => {
+            error!("{}", why);
+            "something went wrong".to_string()
+        }
+    }
 }
 
 pub fn register(commands: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
