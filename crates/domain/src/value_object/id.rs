45333be93afb027e0f173e8d3453c7cc4ac1d6f2
Benj <50514928+mystique09@users.noreply.github.com>
2023-12-13 18:04:49 +0000

    feat: switch to tuple

--- crates/domain/src/value_object/id.rs
+++ crates/domain/src/value_object/id.rs
@@ -1,26 +1,33 @@
 use std::marker::PhantomData;
 
 #[derive(Debug)]
-pub struct Id<T> {
-    id: i64,
-    _phantom: PhantomData<fn() -> T>,
-}
+pub struct Id<T>(i64, PhantomData<fn() -> T>);
 
 impl<T> Id<T> {
     pub fn new(id: i64) -> Self {
-        Self {
-            id,
-            _phantom: PhantomData,
-        }
+        Self(id, PhantomData)
     }
 
     pub fn to_i64(&self) -> i64 {
-        self.id
+        self.0
     }
 }
 
 impl<T> PartialEq for Id<T> {
     fn eq(&self, other: &Self) -> bool {
         self.to_i64() == other.to_i64()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_create_a_new_id_instance() {
+        let id: Id<u32> = Id::new(2);
+
+        assert_eq!(id.to_i64(), 2);
+        assert_eq!(id, Id::new(2));
     }
 }
