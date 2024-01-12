7f8d72abe230452e76008f1b0466e2e3a11a3e47
mystique09 <knightkrypto7@gmail.com>
2024-01-12 15:52:56 +0800

    feat: logger

--- src/bin/void.rs
+++ src/bin/void.rs
@@ -2,12 +2,13 @@ use std::env;
 
 use tokio::runtime::Runtime;
 
-use void_infrastructure::bot;
+use void_infrastructure::{logger, bot};
 
 pub fn main() {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL");
     let port = env::var("PORT").expect("PORT").parse::<u16>().expect("expecting a u16 type");
     let rt = Runtime::new().expect("tokio runtime");
 
+    logger::init_default_logger();
     rt.block_on(bot::run(&database_url, port));
 }
