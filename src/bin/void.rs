22509e0c375348d15c7aa3b403cb8af88cf0e54f
mystique09 <knightkrypto7@gmail.com>
2024-01-12 11:30:56 +0800

    feat: run bot

--- src/bin/void.rs
+++ src/bin/void.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!")
+use std::env;
+
+use tokio::runtime::Runtime;
+
+use void_infrastructure::bot;
+
+pub fn main() {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL");
+    let port = env::var("PORT").expect("PORT").parse::<u16>().expect("expecting a u16 type");
+    let rt = Runtime::new().expect("tokio runtime");
+
+    rt.block_on(bot::run(&database_url, port));
 }
