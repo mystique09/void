fec31ed3ea9493037255904212c9c35763cb477b
mystique09 <knightkrypto7@gmail.com>
2024-01-12 15:52:41 +0800

    feat: bot module

--- crates/discord-bot/src/lib.rs
+++ crates/discord-bot/src/lib.rs
@@ -1,15 +1,16 @@
 use std::sync::{Arc, RwLock};
-
+use log;
 use void_adapter::db::Database;
 
 pub mod handler;
 pub mod commands;
 pub mod hooks;
-pub mod interface;
+pub mod ui;
+pub mod bot;
 
 pub async fn run<D>(_db: RwLock<Arc<D>>, port: u16)
     where
         D: Database
 {
-    println!("[INFO] Running bot on port: {}", port);
+    log::info!("running bot on port: {}", port);
 }
