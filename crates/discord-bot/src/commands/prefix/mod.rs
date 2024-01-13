e461ed7e06e615075c9b84a56842421f8623e771
mystique09 <knightkrypto7@gmail.com>
2024-01-13 17:38:29 +0800

    feat: help command

---
+++ crates/discord-bot/src/commands/prefix/mod.rs
@@ -0,0 +1,31 @@
+pub mod general;
+
+use std::collections::HashSet;
+
+use serenity::all::{Context, Message, UserId};
+use serenity::all::standard::macros::help;
+use serenity::framework::standard::{
+    Args,
+    CommandGroup,
+    CommandResult,
+    help_commands,
+    HelpOptions,
+};
+
+#[help]
+#[max_levenshtein_distance(3)]
+#[lacking_permissions = "Hide"]
+#[lacking_role = "Nothing"]
+#[wrong_channel = "Strike"]
+#[command_not_found_text = "Couldn't find: {}."]
+pub async fn help_cmd(
+    ctx: &Context,
+    msg: &Message,
+    args: Args,
+    help_options: &HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>,
+) -> CommandResult {
+    let _ = help_commands::with_embeds(ctx, msg, args, help_options, groups, owners).await?;
+    Ok(())
+}
