a0fbce99b6933bf0d10c20571f5bf43ac3720409
mystique09 <knightkrypto7@gmail.com>
2024-01-29 11:51:07 +0800

    feat: query database using tokio block_on runtime

--- crates/db/src/lib.rs
+++ crates/db/src/lib.rs
@@ -1,6 +1,7 @@
 use sqlx::PgPool;
 
 use void_adapter::db::Database;
+use void_domain;
 
 #[derive(Debug)]
 pub struct CloudDatabase {
@@ -16,6 +17,8 @@ impl CloudDatabase {
 impl Database for CloudDatabase {}
 
 mod user {
+    use tokio::runtime::Runtime;
+
     use void_application::gateway::repository::user::{DeleteOneError, FindAllError, FindOneError, Map, Repo, SaveError};
     use void_application::identifier::{NewId, NewIdError};
     use void_domain::UserId;
@@ -24,19 +27,81 @@ mod user {
 
     impl Repo for CloudDatabase {
         fn save(&self, map: Map) -> Result<(), SaveError> {
-            todo!("{:?}", map)
+            let rt = Runtime::new().expect("tokio runtime");
+
+            let query = sqlx::query!(
+                r#"
+                INSERT INTO "users" (
+                    id, nickname
+                ) VALUES (
+                    $1, $2
+                );
+                "#,
+                map.user.id(),
+                map.user.nickname()
+            );
+
+            let _ = rt.block_on(query.execute(&self.pool))
+                .map_err(|e| {
+                    if e.to_string().contains("duplicate") {
+                        return SaveError::UserAlreadyExist;
+                    }
+
+                    return SaveError::Connection;
+                })?;
+
+            Ok(())
         }
 
-        fn find_one(&self, id: UserId) -> Result<Map, FindOneError> {
-            todo!("{:?}", id)
+        fn find_one(&self, id: UserId) -> Result<void_domain::User, FindOneError> {
+            let rt = Runtime::new().expect("tokio runtime");
+
+            let query = sqlx::query_as!(
+                void_domain::User,
+                r#"
+                    SELECT * FROM "users"
+                    WHERE id = $1;
+                "#,
+                id.to_i64()
+            );
+
+            let user = rt.block_on(query.fetch_one(&self.pool))
+                .map_err(|_| FindOneError::NotFound)?;
+
+            Ok(user)
         }
 
-        fn find_all(&self) -> Result<Vec<Map>, FindAllError> {
-            todo!()
+        fn find_all(&self) -> Result<Vec<void_domain::User>, FindAllError> {
+            let rt = Runtime::new().expect("tokio runtime");
+
+            let query = sqlx::query_as!(
+               void_domain::User,
+                r#"
+                    SELECT * FROM "users"
+                    GROUP BY id;
+                "#
+            );
+
+            let users = rt.block_on(query.fetch_all(&self.pool))
+                .map_err(|_| FindAllError::Connection)?;
+
+            Ok(users)
         }
 
         fn delete_one(&self, id: UserId) -> Result<(), DeleteOneError> {
-            todo!("{:?}", id)
+            let rt = Runtime::new().expect("tokio runtime");
+
+            let query = sqlx::query!(r#"DELETE FROM "users" WHERE id = $1;"#, id.to_i64());
+
+            let _ = rt.block_on(query.execute(&self.pool))
+                .map_err(|e| {
+                    match e {
+                        sqlx::Error::RowNotFound => DeleteOneError::NotFound,
+                        _ => DeleteOneError::Connection
+                    }
+                })?;
+
+            Ok(())
         }
     }
 
